33dc4ea7b88dcec4924bb43b8aff2e2b95309b64
Kanneti-Mahesh <107530894+Kanneti-Mahesh@users.noreply.github.com>
2023-05-18 17:16:42 +0530

    Update Login.js
    updated

--- src/Login.js
+++ src/Login.js
@@ -114,7 +114,7 @@ function Login() {
       })
 
 
-          let response = await axios.post('http://localhost:8000/admins/login', body,{
+          let response = await axios.post('http://https://mbemsbackend.onrender.com/admins/login', body,{
             headers:{
               'Content-Type':'application/json'
             }
